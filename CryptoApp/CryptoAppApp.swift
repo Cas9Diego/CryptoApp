08e617d8c1242713003e940e07373367e2fab7be
Cas9Diego <DaCastro@outlook.com>
2022-08-29 20:06:41 -0500

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Diego Castro on 29/08/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
